07817041575cc87252bf7250b046e900a851dc98
Hazar Ilhan <b4ti1c@gmail.com>
2022-01-17 19:05:56 +0000

    :sparkles: index: Reimplement logger

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const _ = require('lodash');
 const Joi = require('joi');
 const winston = require('winston');
-const Papertrail = require('winston-papertrail').Papertrail;
-const Elasticsearch = require('winston-elasticsearch');
-const {LoggingWinston: Stackdriver} = require('@google-cloud/logging-winston');
+const {PapertrailTransport} = require('winston-papertrail-transport');
+const {ElasticsearchTransport} = require('winston-elasticsearch');
+const {LoggingWinston: StackdriverTransport} = require('@google-cloud/logging-winston');
 
 
 const consoleTransportSchema = Joi.compile(Joi.object({
@@ -52,7 +52,7 @@ const papertrailTransportSchema = Joi.compile(Joi.object({
     program: Joi.string().required(),
     level: Joi.string().valid('error', 'warn', 'info', 'verbose', 'debug', 'silly'),
     inlineMeta: Joi.boolean().default(true),
-    colorize: Joi.boolean().default(true),
+    colorize: Joi.func(),
     flushOnClose: Joi.boolean().default(true),
     /* Detailed Configuration */
     logFormat: Joi.func(),
@@ -102,174 +102,107 @@ const stackdriverTransportSchema = Joi.compile(Joi.object({
 }).unknown(false));
 
 
-const multiTransportSchema = Joi.compile(Joi.object({
-    console: consoleTransportSchema,
-    file: fileTransportSchema,
-    papertrail: papertrailTransportSchema,
-    elasticsearch: elasticsearchTransportSchema,
-    stackdriver: stackdriverTransportSchema
-}).unknown(false));
+exports.createLogger = function (params) {
+    const {name, serviceType, apiVersion, transports} = Joi.attempt(params, Joi.object({
+        name: Joi.string(),
+        serviceType: Joi.string(),
+        apiVersion: Joi.string(),
+        transports: Joi.object()
+    }));
 
+    const logger = winston.createLogger({level: 'silly'});
 
-class MultiLogger extends winston.Logger {
-    constructor(params) {
-        super();
-
-        Joi.attempt(params, Joi.alternatives().try(
-            Joi.string(),
-            Joi.object({
-                name: Joi.string(),
-                serviceType: Joi.string(),
-                apiVersion: Joi.string(),
-                transports: Joi.object()
-            })
-        ));
-
-        if (_.isString(params))
-            this.setName(params);
-
-        if (_.isObject(params)) {
-            const {name, serviceType, apiVersion, transports} = params;
-
-            if (name) this.setName(name);
-            if (serviceType)this.setServiceType(serviceType);
-            if (apiVersion) this.setApiVersion(apiVersion);
-
-            if (transports) this.addByConfig(transports);
+    _.forEach(transports, (options, transport) => {
+        if (_.isBoolean(options)) {
+            if (!options) return;
+            options = {};
+        } else if (_.isString(options)) {
+            if (options == 'enabled') options = {};
+            else if (options == 'disabled') return;
+            else
+                throw new Error(`Unknown option ${options} for transport ${transport}`);
         }
-    }
 
-    get name() {
-        return this.__uniqueIdentifierName__;
-    }
+        if (transport == 'console') {
+            logger.remove(winston.transports.Console);
 
-    setName(name) {
-        Joi.attempt(name, Joi.string());
-
-        this.__uniqueIdentifierName__ = name;
-    }
-
-    setServiceType(serviceType) {
-        Joi.attempt(serviceType, Joi.string());
-
-        this.serviceType = serviceType;
-    }
-
-    setApiVersion(apiVersion) {
-        Joi.attempt(apiVersion, Joi.string());
-
-        this.apiVersion = apiVersion;
-    }
-
-    addConsole({options = {}, multiple = false} = {}) {
-        if (!multiple && this.transports.console) this.remove(winston.transports.Console);
-
-        const config = Joi.attempt(options, consoleTransportSchema);
-        this.add(winston.transports.Console, config);
-    }
-
-    addFile({options = {}, multiple = false} = {}) {
-        if (!multiple && this.transports.file) this.remove(winston.transports.File);
-
-        const config = Joi.attempt(options, fileTransportSchema);
-        this.add(winston.transports.File, config);
-    }
-
-    addPapertrail({options = {}, multiple = false} = {}) {
-        if (!this.name)
-            throw new Error('A name must be set to the logger before creating papertrail transport');
-
-        if (!multiple && this.transports.Papertrail) this.remove(Papertrail);
-
-        _.defaults(options, {program: this.name});
-
-        const config = Joi.attempt(options, papertrailTransportSchema);
-        this.add(Papertrail, config);
-    }
-
-    addElasticsearch({options = {}, multiple = false} = {}) {
-        if (!multiple && this.transports.Elasticsearch) this.remove(Elasticsearch);
-
-        const config = Joi.attempt(options, elasticsearchTransportSchema);
-        this.add(Elasticsearch, config);
-    }
-
-    addStackdriver({options = {}, multiple = false} = {}) {
-        if (!this.name)
-            throw new Error('A name must be set to the logger before creating stackdriver transport');
-
-        if (!this.serviceType)
-            throw new Error('A serviceType must be set to the logger before creating stackdriver transport');
-
-        if (!this.apiVersion)
-            throw new Error('An apiVersion must be set to the logger before creating stackdriver transport');
-
-        const keyFilename = options.keyFilename || process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-        if (!keyFilename && !options.projectId)
-            throw new Error('Either keyFilename or projectId must be provided in options to create stackdriver transport');
-
-        if (!multiple && this.transports.LoggingWinston) this.remove(LoggingWinston);
-
-        let projectId = null;
-
-        if (keyFilename)
-            projectId = require(keyFilename).project_id;
-
-        _.defaultsDeep(options, {
-            projectId,
-            logName: this.serviceType,
-            serviceContext: {
-                service: this.name,
-                version: this.apiVersion,
-                resourceType: 'api'
-            },
-            keyFilename,
-            labels: {
-                name: this.name,
-                version:  this.apiVersion
-            }
-        });
-
-        const config = Joi.attempt(options, stackdriverTransportSchema);
-        this.add(Stackdriver, config);
-    }
-
-    addTransport({transportName, options = {}} = {}) {
-        switch (transportName) {
-            case 'console': this.addConsole({options}); break;
-            case 'file': this.addFile({options}); break;
-            case 'papertrail': this.addPapertrail({options}); break;
-            case 'elasticsearch': this.addElasticsearch({options}); break;
-            case 'stackdriver': this.addStackdriver({options}); break;
-            default: throw new Error(`Unknown transport ${transportName} is not allowed. Use addCustomTransport method`);
+            const config = Joi.attempt(options, consoleTransportSchema);
+            logger.add(new winston.transports.Console({
+                ...config,
+                format: winston.format.combine(
+                    winston.format.colorize(),
+                    winston.format.simple()
+                )
+            }));
         }
-    }
 
-    addCustomTransport({transport, options = {}} = {}) {
-        this.add(transport, options);
-    }
+        if (transport == 'file') {
+            logger.remove(winston.transports.File);
 
-    addByConfig(conf) {
-        _.forEach(conf, (options, transport) => {
-            if (_.isBoolean(options)) {
-                if (!options) return;
-                options = {};
-            } else if (_.isString(options)) {
-                if (options == 'enabled') options = {};
-                else if (options == 'disabled') return;
-                else
-                    throw new Error(`Unknown option ${options} for transport ${transport}`);
-            }
+            const config = Joi.attempt(options, fileTransportSchema);
+            logger.add(new winston.transports.File(config));
+        }
 
-            if (transport == 'console') this.addConsole({options});
-            if (transport == 'file') this.addFile({options});
-            if (transport == 'papertrail') this.addPapertrail({options});
-            if (transport == 'elasticsearch') this.addElasticsearch({options});
-            if (transport == 'stackdriver') this.addStackdriver({options});
-        });
-    }
+        if (transport == 'papertrail') {
+            if (!name)
+                throw new Error('A name must be set to the logger before creating papertrail transport');
+
+            logger.remove(PapertrailTransport);
+
+            _.defaults(options, {program: name});
+
+            const config = Joi.attempt(options, papertrailTransportSchema);
+            logger.add(new PapertrailTransport(config));
+        }
+
+        if (transport == 'elasticsearch') {
+            logger.remove(ElasticsearchTransport);
+
+            const config = Joi.attempt(options, elasticsearchTransportSchema);
+            logger.add(new ElasticsearchTransport(config));
+        }
+
+        if (transport == 'stackdriver') {
+            if (!name)
+                throw new Error('A name must be set to the logger before creating stackdriver transport');
+
+            if (!serviceType)
+                throw new Error('A serviceType must be set to the logger before creating stackdriver transport');
+
+            if (!apiVersion)
+                throw new Error('An apiVersion must be set to the logger before creating stackdriver transport');
+
+            const keyFilename = options.keyFilename || process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+            if (!keyFilename && !options.projectId)
+                throw new Error('Either keyFilename or projectId must be provided in options to create stackdriver transport');
+
+            logger.remove(StackdriverTransport);
+
+            let projectId = null;
+
+            if (keyFilename)
+                projectId = require(keyFilename).project_id;
+
+            _.defaultsDeep(options, {
+                projectId,
+                logName: serviceType,
+                serviceContext: {
+                    service: name,
+                    version: apiVersion,
+                    resourceType: 'api'
+                },
+                keyFilename,
+                labels: {
+                    name: name,
+                    version:  apiVersion
+                }
+            });
+
+            const config = Joi.attempt(options, stackdriverTransportSchema);
+            logger.add(new StackdriverTransport(config));
+        }
+    });
+
+    return logger;
 }
-
-
-module.exports = MultiLogger;
